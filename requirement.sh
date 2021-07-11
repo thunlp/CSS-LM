9a4a7f91992ead8bf0be7db9420b92158a0563bc
Yusheng <ky200120000@gmail.com>
2021-07-11 16:47:21 +0000

    revision

--- requirement.sh
+++ requirement.sh
@@ -1,11 +1,16 @@
+#install package
 pip install -r requirement.txt
 
+#install apex
 git clone git@github.com:NVIDIA/apex.git
 cd apex
 pip install -v --disable-pip-version-check --no-cache-dir ./
 
 #wget data
-#https://cloud.tsinghua.edu.cn/d/f6a19d5373e74fbfb42d/
+https://cloud.tsinghua.edu.cn/f/690e78d324ee44068857/?dl=1
+
+mv 'index.html?dl=1' download.zip
+unzip download.zip download
 
 scp -r download/opendomain_finetune_noword_10000 data
 scp -r download/openwebtext data
