09cd432d7522545c32075f8981c1bbd24ae36262
Yusheng <ky200120000@gmail.com>
2021-07-11 15:43:14 +0000

    revision

--- requirement.sh
+++ requirement.sh
@@ -5,3 +5,10 @@ cd apex
 pip install -v --disable-pip-version-check --no-cache-dir ./
 
 #wget data
+#https://cloud.tsinghua.edu.cn/d/f6a19d5373e74fbfb42d/
+
+scp -r download/opendomain_finetune_noword_10000 data
+scp -r download/openwebtext data
+scp -r download/roberta-base-768 script
+scp -r download/bert-base-768 script
+
