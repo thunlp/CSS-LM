d1f12ee240c9f9d1f069f2e9da38693d0ffbde5c
Yusheng <ky200120000@gmail.com>
2021-07-20 17:23:28 +0000

    update requirement.sh

--- g.sh
+++
@@ -1,3 +0,0 @@
-git add --all
-git commit -m "revision"
-git push --all
--- requirement.sh
+++ requirement.sh
@@ -6,8 +6,10 @@ git clone git@github.com:NVIDIA/apex.git
 cd apex
 pip install -v --disable-pip-version-check --no-cache-dir ./
 
-#wget data
-https://cloud.tsinghua.edu.cn/f/690e78d324ee44068857/?dl=1
+#wget data:
+#https://cloud.tsinghua.edu.cn/f/690e78d324ee44068857/?dl=1
+
+wget https://cloud.tsinghua.edu.cn/f/690e78d324ee44068857/?dl=1
 
 mv 'index.html?dl=1' download.zip
 unzip download.zip download
