cc97e66daafcfed7bcb9fb9faf3ecd863a6dadf4
Yusheng Su <yushengsu.thu@gmail.com>
2023-07-01 15:52:55 +0800

    delete del.py

--- del.py
+++
@@ -1,7 +0,0 @@
-#from transformers import BertModel, RobertaModel
-
-#q = BertModel.from_pretrained('bert-base-uncased')
-#b = RobertaModel.from_pretrained('roberta-base')
-
-from transformers import BertConfig, BertModel
-model = BertModel.from_pretrained("bert-base-uncased")
